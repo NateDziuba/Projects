a817ff1946d9ec0adfdcdff5ccd53c789154cd36
NateDziuba <nathanieldziuba@gmail.com>
2024-09-03 17:04:50 -0500

    Saved Tecan file.
    Saved the tecan file and am now uploading.

--- DLS_Files/Tecan/import customtkinter.py
+++ DLS_Files/Tecan/import customtkinter.py
@@ -44,8 +44,49 @@ class App(customtkinter.CTk):
         self.tab_view = MyTabView(master=self)
         self.tab_view.grid(row=0, column=0, padx=100, pady=100)
 
+class Dilutions():
+    """Class is used to generate the dilution volumes and masking for the input
+    dilution factors."""
+    def __init__(self, samples):
+        """initialization that takes as an input a dictionary with the key as the sample name and the 
+        value is a tuple of the dilution factor and lot number for the samples. The reqired dilutions will generate
+        another dictionary with the ln+sample name as the key and the value as a tuple of each possible dilution step"""
+        self.samples_dict = samples
+        # samplename: (df, lot number)
+        self.samples_dilutions = None
+        # ln+sample name : (total df, diluent3, sample3, diluent2, sample2, diluent1, sample1, diluent0, sample0)
+        self.dilution_table = None
 
-        
+    def get_sample_names(self):
+        """Returns the keys or sample names"""
+        return self.samples_dict.keys()
+
+    def get_sample_details(self):
+        """Returns alist of the sample name + df + lot number of the sample"""
+        sample_and_ln = []
+        for i in self.samples_dict:
+            sample_and_ln.append(self.samples_dict[i] + "_df = "+ self.sample_dict[i][0]+" _ln_" + self.samples_dict[i][1])
+        return sample_and_ln
+    
+    def generate_dilutions(self):
+        """Function is used to generate the dilutions required for each sample based on the dilution factor.
+        d3 = 901-540,000, d2 = 151-900, d1 = 21-150, d0 = 2-20
+        equation 
+        dilution rational										
+		Pre -3		Pre -2		Pre -1		prep x2 inc spike		
+df		diluent -3	sample -3	diluent-2	sample-2	diluent-1	sample-1	diluent	sample	sample required
+<20	2-20							190	10	200-20
+>20	20-30					270	30	100	100	30
+>30	30-150					280	20	100	100	20
+>150	150-900			280	20	180	120	150	50	20
+>900	900-13500	280	20	222.5	77.5	222.5	77.5	150	50	20
+>13500	13500-160000	285	15	255.2	44.8	255.2	44.8	190	10	15
+>160,000	160000-540000	290	10	281.6	18.4	281.6	18.4	190	10	10
+or max 540,000		285	10	290	10	290	10	190	10	10
+
+        """
+        return
+
 
 
 
